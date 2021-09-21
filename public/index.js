4578ac1a0ea0eb58c246d2cde389c168e0425613
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-09-21 14:51:37 +0200

    Fix missing order id

--- public/index.js
+++ public/index.js
@@ -1,34 +1,36 @@
-const amountInput = document.querySelector('.amount');
-
-paypal.Buttons({
-  createOrder:function() {
-    return fetch('/create-order', {
-      method: "POST",
-      headers: {
-        "Content-Type": 'application/json',
-      },
-      body: JSON.stringify({
-        item: [
-          {
-            id: 1,
-            quatity: 3,
-          },
-          {
-            id: 1,
-            quantity: 2,
-          }
-      ]
+paypal
+  .Buttons({
+    createOrder: function () {
+      return fetch("/create-order", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          items: [
+            {
+              id: 1,
+              quantity: 2,
+            },
+            { id: 2, quantity: 3 },
+          ],
+        }),
       })
-    }).then(res => {
-      if (res.ok) return res.json()
-      return res.json().then(json => Promise.reject(json))
-    }).then(({ id}) => {
-      return id
-    })
-  },
-  onApprove:function(data, actions) {
-    return actions.order.capture().then(function(details) {
-      alert('Trasanction completed by' + details.payer.name.given_name);
-    });
-  }
-}).render('#paypal')
+        .then(res => {
+          if (res.ok) return res.json()
+          return res.json().then(json => Promise.reject(json))
+        })
+        .then(({ id }) => {
+          return id
+        })
+        .catch(e => {
+          console.error(e.error)
+        })
+    },
+    onApprove:function(data, actions) {
+      return actions.order.capture().then(function(details) {
+        alert('Trasanction completed by' + details.payer.name.given_name);
+      });
+    }
+  })
+  .render("#paypal")
